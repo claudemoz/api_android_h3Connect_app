b3a56a96ba0966c69e89a74a6dabf9f4bba7940b
claude tsiangana <claudetsiangana@gmail.com>
2023-02-21 08:48:34 +0100

    correction  entete

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -3,33 +3,35 @@ const { User } = sequelize.models;
 // const bcrypt = require('bcrypt');
 
 exports.createUser = async (req, res, next)=>{
-    const { username, email, password, passwordConfirm } = req.body;
-    // const salt = await bcrypt.genSalt();
-    // const hashedPassword = await bcrypt.hash(password, salt);
+  const { username, email, password, passwordConfirm } = req.body;
+  // const salt = await bcrypt.genSalt();
+  // const hashedPassword = await bcrypt.hash(password, salt);
+  if(!email || !password || !passwordConfirm){
+    res.status(400).json({msg: 'missing data'});
+    return;
+  }
+  if(password !== passwordConfirm){
+    res.status(400).json({msg: 'passwords do not match'});
+    return;
+  }
 
-    try {
-        if(!email || !password || !passwordConfirm){
-            res.status(400).json({msg: 'missing data'});
-        }
-        if(password !== passwordConfirm){
-            res.status(400).json({msg: 'passwords do not match'});
-        }
-
-        const user = await User.findOne({attributes: { exclude: ['password'] },where:{email:email}});
-        if(user){
-            res.status(409).json({msg: 'user already exit'});
-        }
-        const newUser = await User.create({username, email, password })
-        if(newUser){
-            return res.status(200).json({
-                success: true,
-                msg: 'user created'
-            })
-        }
-    } catch (error) {
-        console.log(error);
-        res.status(500).send(error)
+  try {
+    const user = await User.findOne({attributes: { exclude: ['password'] },where:{email:email}});
+    if(!user){
+      const newUser = await User.create({username, email, password })
+      if(newUser){
+        return res.status(200).json({
+          success: true,
+          msg: 'user created'
+        })
+      }
+    }else{
+      return res.status(409).json({msg: 'user already exit'});
     }
+  } catch (error) {
+      console.log(error);
+      res.status(500).send(error)
+  }
     
 }
 
